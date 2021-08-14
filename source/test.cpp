89d6dc8b4af7356be968fbaf2514e51be125ab11
vrodedanya <socurahlem@yandex.ru>
2021-08-14 21:30:30 +0300

    Added new test cases

--- source/test.cpp
+++ source/test.cpp
@@ -1,29 +1,41 @@
 #include "rpn.hpp"
 #include <iostream>
 #include <gtest/gtest.h>
+#include "logger.hpp"
+
 
 // TODO Add more tests
 
 TEST(expression, sum)
 {
+	yesza::logger::turnOff();
+	yesza::logger::setPriority(yesza::logger::Priority::LOW);
 	EXPECT_DOUBLE_EQ(yesza::count("5+10"), 15);
 	EXPECT_DOUBLE_EQ(yesza::count("0+5"), 5);
 	EXPECT_DOUBLE_EQ(yesza::count("10000+12314"), 22314);
 	EXPECT_DOUBLE_EQ(yesza::count(""), 0);
 	EXPECT_DOUBLE_EQ(yesza::count("5.5+ 1.4"), 6.9);
 	EXPECT_DOUBLE_EQ(yesza::count("-5.5+ 1.4"), -4.1);
+	EXPECT_DOUBLE_EQ(yesza::count("-5 * (7+3)"), -50);
+	EXPECT_DOUBLE_EQ(yesza::count("-5 * (-7+3)"), 20);
+	EXPECT_DOUBLE_EQ(yesza::count("-5 * (7-3)"), -20);
+	EXPECT_DOUBLE_EQ(yesza::count("-5 * -(7-3)"), 20);
 }
 TEST(equation, sum)
 {
 	auto eq = yesza::make_equation("x+10");
-	std::cout << "\t\t"<<eq(0) << std::endl;
-	std::cout << "\t\t"<<eq(-10) << std::endl;
-	std::cout << "\t\t"<<eq(5.5) << std::endl;
-	std::cout << "\t\t"<<eq(-10.5) << std::endl;
 	EXPECT_DOUBLE_EQ(eq(0), 10);
 	EXPECT_DOUBLE_EQ(eq(-10), 0);
 	EXPECT_DOUBLE_EQ(eq(5.5), 15.5);
 	EXPECT_DOUBLE_EQ(eq(-10.5), -0.5);
+	eq = yesza::make_equation("x*x");
+	EXPECT_DOUBLE_EQ(eq(0), 0);
+	EXPECT_DOUBLE_EQ(eq(5), 25);
+	EXPECT_DOUBLE_EQ(eq(2), 4);
+	EXPECT_DOUBLE_EQ(eq(10), 100);
+	EXPECT_DOUBLE_EQ(eq(0.1), 0.01);
+	EXPECT_DOUBLE_EQ(eq(-4), 16);
+	EXPECT_DOUBLE_EQ(eq(-4 * 5), 400);
 }
 TEST(expression, min)
 {
